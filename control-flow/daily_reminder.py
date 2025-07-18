4d6c944f3d4c0536eb13b08b204765c604ccccd6
Bethelhem Daniel <82999200+Bethel-21@users.noreply.github.com>
2025-07-18 23:47:48 +0300

    correct according to the checker

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -8,21 +8,21 @@ match priority:
     case "high":
         print("is a high priority")
         if time_bound == "yes":
-            print(f"{task} is a {priority} task that requires immediate attention today!")
+            print(f"Reminder: {task} is a {priority} task that requires immediate attention today!")
         else:
-            print(f"{task} is a {priority} task. Consider completing it when you have free time.")
+            print(f"Reminder: {task} is a {priority} task. Consider completing it when you have free time.")
     case "medium":
         print("is a medium priority")
         if time_bound == "yes":
-             print(f"{task} is a {priority} task that requires immediate attention today!")
+             print(f"Reminder: {task} is a {priority} task that requires immediate attention today!")
         else:
-             print(f"{task} is a {priority} task. Consider completing it when you have free time.")
+             print(f"Reminder: {task} is a {priority} task. Consider completing it when you have free time.")
     case "low":
         print("is a low priority task")
         if time_bound == "yes":
-             print(f"{task} is a {priority} task that requires immediate attention today!")
+             print(f"Reminder: {task} is a {priority} task that requires immediate attention today!")
         else:
-             print(f"{task} is a {priority} task. Consider completing it when you have free time.")
+             print(f"Reminder: {task} is a {priority} task. Consider completing it when you have free time.")
     case _:
         print("Incorrect")
 
