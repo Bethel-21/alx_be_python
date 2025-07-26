1071bff20e91199a3b506c363d265bda1e69e326
Bethelhem Daniel <82999200+Bethel-21@users.noreply.github.com>
2025-07-26 23:58:33 +0300

    Add shoppping_list_manager file

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,37 @@
+
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("What do you want to buy?")
+            shopping_list.append(item)
+            print(f"{item} has been added to the shopping list")
+        elif choice == '2':
+            item = input("What item do you want to remove?")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} has been successfully removed from the list.")
+            else:
+                print(f"The item is not found in the shoping list.")
+            
+        elif choice == '3':
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
