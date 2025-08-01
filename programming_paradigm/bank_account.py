303c34de196092a3f47f78cc98f3639aec6c4e64
unknown <bethelhemdanielfenta@gmail.com>
2025-08-01 11:28:06 +0300

    Rephrase a print statement inside bank_account file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -16,7 +16,7 @@ class BankAccount:
 
 
     def display_balance(self):
-        print(f"Your current balance is ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
 
 
 
