6d57f5a07a89937ccd670bcb4a7edacb583d2318
Bethelhem Daniel <82999200+Bethel-21@users.noreply.github.com>
2025-08-03 10:14:26 +0300

    Made a minor change accoring to the checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -16,7 +16,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
     def test_both_negative_addition(self):
         self.assertEqual(self.calc.add(-3,-4), -7)
-    def test_normal_subtraction(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(6,5), 1)
     def test_bigger_number_negative_subtraction(self):
         self.assertEqual(self.calc.subtract(-10, 7), -17)
@@ -24,13 +24,13 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(10, -7), 17)
     def test_both_negative_subtraction(self):
         self.assertEqual(self.calc.subtract(-4,-1), -3)
-    def test_positive_multiplication(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3,4), 12)
     def test_one_negative_multiplication(self):
         self.assertEqual(self.calc.multiply(-3, 4), -12)
     def test_both_negative_multiplication(self):
         self.assertEqual(self.calc.multiply(-3,-4), 12)
-    def test_normal_division(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10,5), 2)
     def test_byzero_division(self):
         self.assertEqual(self.calc.divide(4,0), "None")
